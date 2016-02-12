36ee543ddf1c5a877bc79354599cea6f5ff82fc5
Hortoni Gomes <hortonigomes@gmail.com>
2016-02-11 22:38:11 -0300

    add views

--- src/main/java/com/manolosmobile/springboot/controller/GreetingController.java
+++ src/main/java/com/manolosmobile/springboot/controller/GreetingController.java
@@ -2,48 +2,53 @@ package com.manolosmobile.springboot.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.manolosmobile.springboot.model.Greeting;
 
-@RestController
+@Controller
 public class GreetingController {
+
+	@RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 	
-	private static final String template = "Hello, %s";
-	private final AtomicLong counter = new AtomicLong();
-	private List<Greeting> greetings = new ArrayList<>();
+	@RequestMapping("/greetings")
+    public String greetings(Model model) {
+		
+        model.addAttribute("greetings", getListGreetings());
+        return "greetings";
+    }
 	
-	@RequestMapping("/create")
-	public Greeting createGreeting(@RequestParam(value="name", required=true) String name) {
-		Greeting greeting = new Greeting(counter.incrementAndGet(), String.format(template, name)); 
-		greetings.add(greeting);
-		return greeting;
+	
+	private List<String> getList() {
+
+		List<String> list = new ArrayList<String>();
+		list.add("List A");
+		list.add("List B");
+		list.add("List C");
+		list.add("List D");
+		list.add("List 1");
+		list.add("List 2");
+		list.add("List 3");
+
+		return list;
+
 	}
 	
-	@RequestMapping("/get")
-	public Greeting getGreeting(@RequestParam(value="id", required=true) long id) {
-		for (Greeting greeting : greetings) {
-			if (greeting.getId() == id) {
-				return greeting;
-			}
-		}
-		return null;
+	private List<Greeting> getListGreetings() {
+		List<Greeting> list = new ArrayList<Greeting>();
+		
+		list.add(new Greeting(1, "thiago"));
+		list.add(new Greeting(2, "ph"));
+		list.add(new Greeting(3, "hortoni"));
+
+		return list;
 	}
-	
-	@RequestMapping("/get/{id}")
-	public Greeting getGreeting2(@PathVariable(value="id") long id) {
-		for (Greeting greeting : greetings) {
-			if (greeting.getId() == id) {
-				return greeting;
-			}
-		}
-		return null;
-	}
-	
-	
 }
