086390719940a521e8426e87a8817cfd320b4f59
Thiago Porciúncula <tfcporciuncula@gmail.com>
2016-02-16 10:57:21 -0300

    identation fixes

--- src/main/java/com/manolosmobile/springboot/controller/GreetingController.java
+++ src/main/java/com/manolosmobile/springboot/controller/GreetingController.java
@@ -1,7 +1,6 @@
 package com.manolosmobile.springboot.controller;
 
 import com.manolosmobile.springboot.model.Greeting;
-import org.springframework.core.env.SystemEnvironmentPropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +13,8 @@ import java.util.List;
 @Controller
 public class GreetingController {
 
+    private RestTemplate restTemplate = new RestTemplate();
+
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
@@ -22,16 +23,17 @@ public class GreetingController {
 
     @RequestMapping("/greetings")
     public String greetings(Model model) {
-        RestTemplate restTemplate = new RestTemplate();
-
-        List<Greeting> greetings = Arrays.asList(restTemplate.getForObject("http://localhost:" + getPort() + "/getGreetingList", Greeting[].class));
+        List<Greeting> greetings = Arrays.asList(restTemplate.getForObject(getServiceUrl() + "/getGreetingList", Greeting[].class));
         model.addAttribute("greetings", greetings);
         return "greetings";
     }
 
-    private String getPort() {
-        String port = System.getenv("port");
-        return (port == null) ? "8080" : port;
+    private String getServiceUrl() {
+        String port = System.getenv("PORT");
+        if (port == null) {
+            port = "8080";
+        }
+        return "http://localhost:" + port;
     }
 
 }
