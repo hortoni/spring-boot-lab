b86524b1634671ad576377c8418552998521add5
hortoni <hortonny@hotmail.com>
2016-02-16 12:07:09 -0300

    import fix

--- src/main/java/com/manolosmobile/springboot/controller/GreetingController.java
+++ src/main/java/com/manolosmobile/springboot/controller/GreetingController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
